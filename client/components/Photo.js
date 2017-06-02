a8f5373aa64c5b8e87d8557a53e613e7b82a3765
Srivatsa S <srivatsashankaran@gmail.com>
2017-06-02 12:32:40 +0530

    updating state with reducers

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -7,36 +7,33 @@ const Photo = React.createClass({
     const { post, i, comments } = this.props;
     return (
       <figure className="grid-figure">
-         <div className="grid-photo-wraps">
-           <Link to={`/view/${post.code}`}>
-             <img src={post.display_src} alt={post.caption} className="grid-photo"/>
-           </Link>
+        <div className="grid-photo-wrap">
+          <Link to={`/view/${post.code}`}>
+            <img src={post.display_src} alt={post.caption} className="grid-photo" />
+          </Link>
 
-           <CSSTransitionGroup transitionName = "like"
-             transitionEnterTimeOut={500}
-             transitionLeaveTimeOut={500}>
-             <span key={post.likes} className="likes-heart">
-               {post.likes}
-             </span>
-           </CSSTransitionGroup>
-         </div>
+          <CSSTransitionGroup transitionName="like" transitionEnterTimeout={500} transitionLeaveTimeout={500}>
+            <span key={post.likes} className="likes-heart">{post.likes}</span>
+          </CSSTransitionGroup>
 
-         <figcaption>
-            <p>{post.caption}</p>
-            <div className="control-buttons">
-              <button className="likes">&hearts; {post.likes}</button>
-              <Link className="button" to={`/view/${post.code}`}>
-                <span className="comment-count">
-                  <span className="speech-bubble"></span>
-                  {comments[post.code] ? comments[post.code].length : 0}
-                </span>
-              </Link>
-            </div>
-         </figcaption>
+        </div>
+
+        <figcaption>
+          <p>{post.caption}</p>
+          <div className="control-buttons">
+            <button onClick={this.props.increment.bind(null, i)} className="likes">&hearts; {post.likes}</button>
+            <Link className="button" to={`/view/${post.code}`}>
+              <span className="comment-count">
+                <span className="speech-bubble"></span>
+                {comments[post.code] ? comments[post.code].length : 0 }
+              </span>
+            </Link>
+          </div>
+        </figcaption>
 
       </figure>
     )
   }
 });
 
-export default Photo
+export default Photo;
