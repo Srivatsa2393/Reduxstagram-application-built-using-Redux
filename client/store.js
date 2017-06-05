77468cfad38670082578c2c95400eb1bc8f39c40
Srivatsa S <srivatsashankaran@gmail.com>
2017-06-05 14:56:17 +0530

    Using of redux dev tools

--- client/store.js
+++ client/store.js
@@ -1,4 +1,4 @@
-import { createStore, compse } from 'redux';
+import { createStore, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
@@ -15,8 +15,12 @@ const defaultState = {
   comments
 };
 
+const enhancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
 //make the store
-const store = createStore(rootReducer, defaultState);
+const store = createStore(rootReducer, defaultState, enhancers);
 
 //finally we need the history
 export const history = syncHistoryWithStore(browserHistory, store);
@@ -24,7 +28,7 @@ export const history = syncHistoryWithStore(browserHistory, store);
 if(module.hot){
   module.hot.accept('./reducers/', () => {
     const nextRootReducer = require('./reducers/index').default;
-    store.replaceReducer(nextRootReducer); 
+    store.replaceReducer(nextRootReducer);
   });
 }
 
